b427e6628c74c4aa22b58a999f74e4d8b50322a3
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2026-03-25 14:21:35 +0100

    avoiding suspense by including an export to set dynamic to force-dynamic

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-export const dynamic = 'force-dynamic';
 
 export default function LoginPage() {
   const router = useRouter();
